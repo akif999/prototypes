34cd88b65fe4cb5da8e39f40f99b702316683fef
akif999 <aki01nf28@gmail.com>
2017-09-09 23:20:37 +0900

    implement formatFunc

--- go/canlog/app/main.go
+++ go/canlog/app/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/akif999/prototypes/go/canlog"
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/akif999/prototypes/go/canlog"
 )
 
 func main() {
@@ -11,17 +13,19 @@ func main() {
 	filename := os.Args[1]
 	ids := []string{"1F3", "710", "1C8"}
 
-	c := canlog.New()
+	c := canlog.NewCanLog()
 	err := c.Parse(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	ca := canlog.PickRecord(c, ids)
-	ca.PrintLog(canlog.WHOLE)
+	s := ca.String()
+	fmt.Print(s)
+
+	fmt.Println()
 
 	c = canlog.DelRecord(c, ids)
-	c.PrintLog(canlog.WHOLE)
-	c.PrintLog(canlog.DATA)
-
+	s = c.String()
+	fmt.Print(s)
 }
--- go/canlog/canlog.go
+++ go/canlog/canlog.go
@@ -8,13 +8,9 @@ import (
 	"strings"
 )
 
-const (
-	WHOLE = iota
-	DATA
-)
-
 type CanLog struct {
-	logs []*log
+	logs     []*log
+	formater FormatFunc
 }
 
 // Canlog型はCanlogに合わせたデータ構造を持ったオブジェクト
@@ -25,14 +21,17 @@ type log struct {
 	dir    string
 	stat   string
 	dlc    int
-	data   []int64
+	data   []byte
 	remain string
 }
 
+type FormatFunc func(time float64, ch, id, dir string, dlc int, data []byte) string
+
 // Newは、新たにCanlogオブジェクトを作成する
-func New() CanLog {
-	var c CanLog
-	return c
+func NewCanLog() *CanLog {
+	return &CanLog{
+		formater: formatLog,
+	}
 }
 
 // Parseは、ファイルを解析してCanlogオブジェクトを作成する
@@ -57,8 +56,8 @@ func (c *CanLog) Parse(filename string) error {
 			l.stat = fs[4]
 			l.dlc, _ = strconv.Atoi(fs[5])
 			for _, f := range fs[6 : l.dlc+6] {
-				d, _ := strconv.ParseInt(f, 16, 32)
-				l.data = append(l.data, d)
+				d, _ := strconv.ParseUint(f, 16, 32)
+				l.data = append(l.data, byte(d))
 			}
 			l.remain = strings.Join(fs[l.dlc+7:l.dlc+15], " ")
 			c.logs = append(c.logs, l)
@@ -68,8 +67,8 @@ func (c *CanLog) Parse(filename string) error {
 }
 
 // PickRecordは、引数で渡したCanlogオブジェクトからidsで渡したIDが存在するレコードのみを抽出したCanlogオブジェクトを返す
-func PickRecord(c CanLog, ids []string) CanLog {
-	var nc CanLog
+func PickRecord(c *CanLog, ids []string) *CanLog {
+	nc := NewCanLog()
 	for _, r := range c.logs {
 		if isContains(r.id, ids) {
 			nc.logs = append(nc.logs, r)
@@ -79,8 +78,8 @@ func PickRecord(c CanLog, ids []string) CanLog {
 }
 
 // DelRecordは、引数で渡したCanlogオブジェクトからidsで渡したIDが存在するレコードを削除したCanlogオブジェクトを返す
-func DelRecord(c CanLog, ids []string) CanLog {
-	var nc CanLog
+func DelRecord(c *CanLog, ids []string) *CanLog {
+	nc := NewCanLog()
 
 	for _, r := range c.logs {
 		if !isContains(r.id, ids) {
@@ -100,31 +99,26 @@ func isContains(tgt string, arr []string) bool {
 	return false
 }
 
-type formatFunc func(time, ch, id, dir, dlc string, data []byte) string
-
 // PrintLogは、CanLogオブジェクトを1レコードずつフォーマットして標準出力へ出力する
-func (c *CanLog) PrintLog(opt int) {
-	for _, r := range c.logs {
-		switch opt {
-		case WHOLE:
-			s := DataToString(r, " ")
-			fmt.Printf("%06f %s %03s %s %X %s\n", r.time, r.ch, r.id, r.dir, r.dlc, s)
-		case DATA:
-			s := DataToString(r, "")
-			fmt.Printf("%s\n", s)
-		default:
-			s := DataToString(r, " ")
-			fmt.Printf("%06f %s %03s %s %X %s\n", r.time, r.ch, r.id, r.dir, r.dlc, s)
-		}
+func (c *CanLog) String() string {
+	s := ""
+	for _, l := range c.logs {
+		s += c.formater(l.time, l.ch, l.id, l.dir, l.dlc, l.data)
 	}
+	return s
+}
+
+func formatLog(time float64, ch, id, dir string, dlc int, data []byte) string {
+	s := BytesToSeparatedString(data, " ")
+	return fmt.Sprintf("%06f %s %03s %s %X %s\n", time, ch, id, dir, dlc, s)
 }
 
 // DataToStringは、レコードの配列データから、sepで区切った文字列へ変換する
-func DataToString(record *log, sep string) string {
+func BytesToSeparatedString(bytes []byte, sep string) string {
 	var data []string
 
-	for _, d := range record.data {
-		data = append(data, fmt.Sprintf("%02X", d))
+	for _, b := range bytes {
+		data = append(data, fmt.Sprintf("%02X", b))
 	}
 	return strings.Join(data, sep)
 }
