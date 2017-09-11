a027f5153cd5d78c551b06e21aec0e6690614c65
akif999 <aki01nf28@gmail.com>
2017-09-11 22:23:12 +0900

    Add new module canlog

---
+++ perl/canlog/README.md
@@ -0,0 +1,22 @@
+# NAME
+
+canlog - It's new $module
+
+# SYNOPSIS
+
+    use canlog;
+
+# DESCRIPTION
+
+canlog is ...
+
+# LICENSE
+
+Copyright (C) akif999.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+akif999 <aki01nf28@gmail.com>
---
+++ perl/canlog/lib/canlog.pm
@@ -0,0 +1,39 @@
+package canlog;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+canlog - It's new $module
+
+=head1 SYNOPSIS
+
+    use canlog;
+
+=head1 DESCRIPTION
+
+canlog is ...
+
+=head1 LICENSE
+
+Copyright (C) akif999.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+akif999 E<lt>aki01nf28@gmail.comE<gt>
+
+=cut
+
