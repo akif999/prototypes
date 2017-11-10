84142a0636d3dd2530f65c3f5366d128556f074b
akif999 <aki01nf28@gmail.com>
2017-11-10 22:42:24 +0900

    implement test filter

--- sh/test.sh
+++ sh/test.sh
@@ -1,3 +1,14 @@
-#!/bin/zsh
+#!/bin/bash
 
-echo "hoge"
+while read line
+do
+    str=($line)
+    if [ ${str[1]} != '1' ] && [ ${str[1]} != '2' ]; then
+        continue
+    fi
+    for ((i = 0; i < ${str[5]}+6; i++))
+    do
+        echo -n "${str[$i]} "
+    done
+    echo
+done < ${1}
