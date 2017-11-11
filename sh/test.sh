21ad22abdaf8e691d339399a13b808d44412f482
akif999 <aki01nf28@gmail.com>
2017-11-11 15:48:44 +0900

    add filter by hex

--- sh/test.sh
+++ sh/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # check number of arguments
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
     echo "1 arguments required, but has $#"
     exit 1
 fi
@@ -13,9 +13,13 @@ do
     if [ ${str[1]} != '1' ] && [ ${str[1]} != '2' ]; then
         continue
     fi
+    if [ ${str[2]} != $2 ]; then
+        continue
+    fi
     for ((i = 0; i < ${str[5]}+6; i++))
     do
-        echo -n "${str[$i]} "
+            echo -n "${str[$i]} "
     done
     echo
 done < ${1}
+exit 0
