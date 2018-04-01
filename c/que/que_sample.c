0929403758fc54d669ae379d29528ba50ece27f6
akif999 <aki01nf28@gmail.com>
2018-04-01 23:09:03 +0900

    update sample source-code

--- c/que/que_sample.c
+++ c/que/que_sample.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 #define BUFFER_SIZE 5
+#define BUFFER_EMPTY 0
+#define BUFFER_NOT_EMPTY 1
 
 typedef struct
 {
@@ -12,11 +14,12 @@ typedef struct
 void init_ring_buffer(ring_buffer *r_buf);
 void dump_buffer(ring_buffer r_buf);
 void enqueue(ring_buffer *r_buf, unsigned short elem);
-unsigned short dequeue(ring_buffer *r_buf);
+unsigned char dequeue(ring_buffer *r_buf, unsigned short *elem);
 
 int main (void)
 {
     ring_buffer r_buf;
+    unsigned short tmp;
 
     init_ring_buffer(&r_buf);
     enqueue(&r_buf, 0x1111);
@@ -27,9 +30,10 @@ int main (void)
     enqueue(&r_buf, 0x6666);
     enqueue(&r_buf, 0x7777);
     dump_buffer(r_buf);
-    printf("deque  : %04X\n", dequeue(&r_buf));
-    printf("deque  : %04X\n", dequeue(&r_buf));
-    printf("deque  : %04X\n", dequeue(&r_buf));
+    while(dequeue(&r_buf, &tmp))
+    {
+        printf("deque  : %04X\n", tmp);
+    }
     dump_buffer(r_buf);
 }
 
@@ -50,18 +54,43 @@ void enqueue(ring_buffer *r_buf, unsigned short elem)
     if (r_buf->tail == &r_buf->buf[BUFFER_SIZE - 1])
     {
         r_buf->tail = &r_buf->buf[0];
-    } else {
+        if (r_buf->head == r_buf->tail)
+        {
+            r_buf->head++;
+        }
+    }
+    else
+    {
         r_buf->tail++;
+        if (r_buf->head == r_buf->tail)
+        {
+            r_buf->head++;
+        }
     }
 }
 
-unsigned short dequeue(ring_buffer *r_buf)
+unsigned char dequeue(ring_buffer *r_buf, unsigned short *elem)
 {
     unsigned short ret;
 
-    ret = *r_buf->head;
-    *r_buf->head = 0x0000;
-    r_buf->head++;
+    ret = BUFFER_NOT_EMPTY;
+    if (r_buf->head == r_buf->tail)
+    {
+        ret = BUFFER_EMPTY;
+    }
+    else
+    {
+        *elem = *r_buf->head;
+        *r_buf->head = 0x0000;
+        if (r_buf->head == &r_buf->buf[BUFFER_SIZE - 1])
+        {
+            r_buf->head = & r_buf->buf[0];
+        }
+        else
+        {
+            r_buf->head++;
+        }
+    }
     return ret;
 }
 
