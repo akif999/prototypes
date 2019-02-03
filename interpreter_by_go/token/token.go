8c528676412a3444701ca62423e837d622655a13
akif999 <aki01nf28@gmail.com>
2019-02-03 15:02:10 +0900

    implement token types and contants

--- interpreter_by_go/token/token.go
+++ interpreter_by_go/token/token.go
@@ -1,7 +1,34 @@
-package main
+package token
 
-import "fmt"
+type TokentType string
 
-func main() {
-	fmt.Println("vim-go")
+type Token struct {
+	Type    TokenType
+	Literal string
 }
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifier + Literal
+	IDENT = "IDENT" // add, foobar, x, y ...
+	INT   = "INT"   // 123456
+
+	// Operator
+	ASSIGN = "-"
+	PLUS   = "+"
+
+	// Delimiter
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keyword
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
