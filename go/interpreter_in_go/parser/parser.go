362c56ab866c0667d4bb2e2668e23ba2a62f35fd
akif999 <aki01nf28@gmail.com>
2019-07-03 11:26:13 +0900

    p109-111(fixed TODO)

--- go/interpreter_in_go/parser/parser.go
+++ go/interpreter_in_go/parser/parser.go
@@ -137,8 +137,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	// TODO: now, skip formulas until get semicolon
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -150,8 +153,9 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	// TODO: now, skip formulas until get semicolon
-	for !p.curTokenIs(token.SEMICOLON) {
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
