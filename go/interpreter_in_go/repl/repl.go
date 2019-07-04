054b401a506608821a5e361e92455e3a60fcc473
akif999 <aki01nf28@gmail.com>
2019-07-04 14:22:52 +0900

    p112-114(advanced repl)

--- go/interpreter_in_go/repl/repl.go
+++ go/interpreter_in_go/repl/repl.go
@@ -6,8 +6,21 @@ import (
 	"io"
 
 	"github.com/akif999/prototypes/go/interpreter_in_go/lexer"
-	"github.com/akif999/prototypes/go/interpreter_in_go/token"
+	"github.com/akif999/prototypes/go/interpreter_in_go/parser"
 )
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 func main() {
 	fmt.Println("vim-go")
@@ -27,9 +40,25 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops!! We ran into some monkey business here!\n")
+	io.WriteString(out, " parse errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
