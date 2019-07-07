66693f56da137e4ed044a57c5aceed6ece158646
akif999 <aki01nf28@gmail.com>
2019-07-07 15:20:05 +0900

    p144-149(add evaluator)

--- go/interpreter_in_go/object/object.go
+++ go/interpreter_in_go/object/object.go
@@ -19,10 +19,15 @@ type Boolean struct {
 
 type Null struct{}
 
+type ReturnValue struct {
+	Value Object
+}
+
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
@@ -33,3 +38,6 @@ func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 
 func (n *Null) Inspect() string  { return "null" }
 func (n *Null) Type() ObjectType { return NULL_OBJ }
+
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
