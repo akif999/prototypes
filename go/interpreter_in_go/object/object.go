0a84520b420d6e69abfcd5bd44e53b35e1862c8d
akif999 <aki01nf28@gmail.com>
2019-07-08 17:35:33 +0900

    p159-169(add function call)

--- go/interpreter_in_go/object/object.go
+++ go/interpreter_in_go/object/object.go
@@ -1,6 +1,12 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/akif999/prototypes/go/interpreter_in_go/ast"
+)
 
 type ObjectType string
 
@@ -23,6 +29,12 @@ type ReturnValue struct {
 	Value Object
 }
 
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
 type Error struct {
 	Message string
 }
@@ -32,6 +44,7 @@ const (
 	BOOLEAN_OBJ      = "BOOLEAN"
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	FUNCTION_OBJ     = "FUNCTION"
 	ERROR_OBJ        = "ERROR"
 )
 
@@ -46,6 +59,25 @@ func (n *Null) Type() ObjectType { return NULL_OBJ }
 
 func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
 
 func (e *Error) Inspect() string  { return "ERROR:" + e.Message }
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
