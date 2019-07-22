c860c92400580c15d538ee1b394d261eda31403a
akif999 <aki01nf28@gmail.com>
2019-07-22 15:37:56 +0900

    add Rust sample project

---
+++ rust/binary_tree/src/main.rs
@@ -0,0 +1,44 @@
+#[derive(Debug)]
+enum BinaryTree<T> {
+    Empty,
+    NonEmpty(Box<TreeNode<T>>),
+}
+
+#[derive(Debug)]
+struct TreeNode<T> {
+    element: T,
+    left: BinaryTree<T>,
+    right: BinaryTree<T>,
+}
+
+fn main() {
+    let mut tree = BinaryTree::Empty;
+    tree.add(1);
+    tree.add(3);
+    tree.add(5);
+    tree.add(7);
+    tree.add(1);
+    tree.add(3);
+    println!("tree: {:?}", tree);
+}
+
+impl<T: Ord> BinaryTree<T> {
+    fn add(&mut self, value: T) {
+        match *self {
+            BinaryTree::Empty => {
+                *self = BinaryTree::NonEmpty(Box::new(TreeNode {
+                    element: value,
+                    left: BinaryTree::Empty,
+                    right: BinaryTree::Empty,
+                }))
+            }
+            BinaryTree::NonEmpty(ref mut node) => {
+                if value <= node.element {
+                    node.left.add(value);
+                } else {
+                    node.right.add(value);
+                }
+            }
+        }
+    }
+}
