16b4f21d341a0c35a6539760cec9dab4f2c1a5d2
akif999 <aki01nf28@gmail.com>
2019-07-23 15:58:34 +0900

    update go/digest_auth

--- go/digest_auth/server/server.go
+++ go/digest_auth/server/server.go
@@ -1,30 +1,36 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"log"
 	"net/http"
+
+	auth "github.com/abbot/go-http-auth"
 )
 
 const (
-	basicAuthUser     = "user"
-	basicAuthPassword = "pass"
+	digestAuthUser     = "user"
+	digestAuthPassword = "pass"
 )
 
+func secret(user, realm string) string {
+	if user == digestAuthUser {
+		// return fmt.Sprintf("%x", md5.Sum([]byte(digestAuthPassword)))
+		return "1a1dc91c907325c69271ddf0c944bc72"
+	}
+	return ""
+}
+
+func handle(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
+	fmt.Fprintf(w, "<html><body><h1>Hello, %s!</h1></body></html>", r.Username)
+}
+
 func main() {
-	http.HandleFunc("/basic",
-		func(w http.ResponseWriter, r *http.Request) {
-			if user, pass, ok := r.BasicAuth(); !ok || user != basicAuthUser || pass != basicAuthPassword {
-				w.Header().Add("WWW-Authenticate", `Basic realm="my private area"`)
-				w.WriteHeader(http.StatusUnauthorized)
-				http.Error(w, "Not authorized", 401)
-				return
-			}
-			_, err := fmt.Fprintf(w, "Authed\n")
-			if err != nil {
-				log.Fatal(err)
-			}
-		},
-	)
-	log.Println(http.ListenAndServe(":18888", nil))
+	s := fmt.Sprintf("%x", md5.Sum([]byte(digestAuthPassword)))
+	fmt.Println(s)
+
+	authenticator := auth.NewDigestAuthenticator("Secret Zone", secret)
+	http.HandleFunc("/", authenticator.Wrap(handle))
+	log.Println(http.ListenAndServe(":8080", nil))
 }
