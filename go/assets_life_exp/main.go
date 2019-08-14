75c40e1acc823e518d157e1c31c8d247aaf6c51d
akif999 <aki01nf28@gmail.com>
2019-08-14 11:20:57 +0900

    Add exapmle of Go pkg (assets-life)

---
+++ go/assets_life_exp/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/akif999/prototypes/go/assets_life_exp/assets"
+)
+
+func main() {
+	f, err := assets.Root.Open("/hoge.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, _ := ioutil.ReadAll(f)
+	fmt.Println(string(b))
+}
